90f474fc1428cd88e3449f5261ec86752ec627f5
ichwanul <87606136+ichwanul@users.noreply.github.com>
2024-03-08 23:43:04 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,29 @@
+{
+  "name": "esteah",
+  "version": "1.0.0",
+  "description": "election survey tool",
+  "main": "index.js",
+  "scripts": {
+    "test": "test"
+  },
+  "repository": {
+    "type": "git",
+    "url": "hhttps://github.com/ichwanul/esteah.git"
+  },
+  "keywords": [
+    "esteah",
+    "gelas"
+  ],
+  "author": "amapa",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/ichwanul/esteah/issues"
+  },
+  "homepage": "https://github.com/ichwanul/esteah#readme",
+  "dependencies": {
+    "cubitt":"^1.0.3",
+    "salaks":"^1.0.0",
+    "guessdc":"^1.0.0",
+    "matchax":"^1.0.1"
+  }
+}
